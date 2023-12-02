3a7138d1d7c81feded370aef87409d168f14bba1
gprem09 <premnathgnanavel150@gmail.com>
2023-12-01 18:41:00 -0800

    Normalization method

--- CMPT354_P2.sql
+++ CMPT354_P2.sql
@@ -16,3 +16,38 @@ INSERT INTO R (S, A, C, T, D) VALUES
 ('Student1', 'Address1', 'Course2', 'Department2', 'Teacher2'),
 ('Student2', 'Address2', 'Course1', 'Department1', 'Teacher1'),
 ('Student2', 'Address2', 'Course3', 'Department3', 'Teacher3');
+
+-- Normalization structure
+CREATE TABLE Students (
+  S char(20) PRIMARY KEY,
+  A char(20)
+);
+
+CREATE TABLE Teachers (
+  T char(20) PRIMARY KEY,
+  D char(20)
+);
+
+CREATE TABLE Courses (
+  C char(20),
+  S char(20),
+  T char(20),
+  PRIMARY KEY (C, S),
+  FOREIGN KEY (S) REFERENCES Students(S),
+  FOREIGN KEY (T) REFERENCES Teachers(T)
+);
+
+INSERT INTO Students (S, A) VALUES
+('Student1', 'Address1'),
+('Student2', 'Address2');
+
+INSERT INTO Teachers (T, D) VALUES
+('Teacher1', 'Department1'),
+('Teacher2', 'Department2'),
+('Teacher3', 'Department3');
+
+INSERT INTO Courses (C, S, T) VALUES
+('Course1', 'Student1', 'Teacher1'),
+('Course2', 'Student1', 'Teacher2'),
+('Course1', 'Student2', 'Teacher1'),
+('Course2', 'Student2', 'Teacher3');
